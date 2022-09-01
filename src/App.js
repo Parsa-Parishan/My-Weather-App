3c91c828f93aa45af818468c55873aa2283b31ed
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-01 15:53:12 -0400

    fetch current weather and forecast api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useState, useEffect } from "react";
+import Search from "./components/Search.js";
+import { weatherAPIURL, weatherAPIKey } from "./data/data";
+
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleSearch = (searchData) => {
+    const [latitude, longitude] = searchData.value.split(" ");
+    console.log(searchData);
+    const currentWeatherFetch = fetch(
+      `${weatherAPIURL}/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPIKey}&units=metric`
+    );
+
+    const forecast = fetch(
+      `${weatherAPIURL}/forecast?lat=${latitude}&lon=${longitude}&appid=${weatherAPIKey}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecast])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        const forecastResponse = await response[1].json();
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="App">
+      <Search handleSearch={handleSearch} />
     </div>
   );
 }
