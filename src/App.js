05d38772366adc90ac366597d626b58b168cc266
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-01 16:45:08 -0400

    format

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Search from "./components/Search.js";
 import { weatherAPIURL, weatherAPIKey } from "./data/data";
 import Current from "./components/Current";
