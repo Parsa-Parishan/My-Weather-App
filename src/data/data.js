3a27f1329abd1641e8733ca73cc4f83c0841087e
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-01 15:45:50 -0400

    fetch api

---
+++ src/data/data.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1fd73e04a1msh6f9265bf2683a87p10a0a6jsnc8142c86f857",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const URLToFetch = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const weatherAPIURL = "https://api.openweathermap.org/data/2.5";
+export const weatherAPIKey = "03cb91f3eb8e293697b069664f75bb43";
