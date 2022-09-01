7d6f95807f60af997f6d35aad6741750b14f9af1
Parsa Parishan <pparsaparishan@yahoo.com>
2022-09-01 15:48:47 -0400

    fetch geoAPI and render AsyncPaginate

---
+++ src/components/Search.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, URLToFetch } from "../data/data";
+
+export default function Search({ handleSearches }) {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (input) => {
+    return fetch(
+      `${URLToFetch}/cities?limit=10&minPopulation=10000&namePrefix=${input}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handleChange = (searchData) => {
+    setSearch(() => searchData);
+    handleSearches(searchData);
+  };
+
+  return (
+    <div>
+      <AsyncPaginate
+        placeholder="Search for a city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleChange}
+        loadOptions={loadOptions}
+      />
+    </div>
+  );
+}
